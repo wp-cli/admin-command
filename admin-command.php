a9efe26760ad7ab9770607f639be0b07b7ac282a
Pascal Birchler <pascalb@google.com>
2026-04-15 16:10:19 +0200

    Use closure in command file

--- admin-command.php
+++ admin-command.php
@@ -4,20 +4,24 @@ if ( ! class_exists( 'WP_CLI' ) ) {
 	return;
 }
 
-/**
- * Open /wp-admin/ in a browser.
- */
-function wp_cli_admin_command() {
-	switch ( strtoupper( substr( PHP_OS, 0, 3 ) ) ) {
-		case 'DAR':
-			$exec = 'open';
-			break;
-		case 'WIN':
-			$exec = 'start ""';
-			break;
-		default:
-			$exec = 'xdg-open';
-	}
-	passthru( $exec . ' ' . escapeshellarg( admin_url() ) );
+$wpcli_core_autoloader = __DIR__ . '/vendor/autoload.php';
+if ( file_exists( $wpcli_core_autoloader ) ) {
+	require_once $wpcli_core_autoloader;
 }
-WP_CLI::add_command( 'admin', 'wp_cli_admin_command' );
+
+WP_CLI::add_command(
+	'admin',
+	static function () {
+		switch ( strtoupper( substr( PHP_OS, 0, 3 ) ) ) {
+			case 'DAR':
+				$exec = 'open';
+				break;
+			case 'WIN':
+				$exec = 'start ""';
+				break;
+			default:
+				$exec = 'xdg-open';
+		}
+		passthru( $exec . ' ' . escapeshellarg( admin_url() ) );
+	}
+);
